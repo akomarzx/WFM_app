28643515f37145e13b0d4a90468ed0e2c023fb65
Ronald Jr Ombao <rombao@my.centennialcollege.ca>
2022-05-11 14:42:05 -0400

    Additional Setup

--- app.js
+++ app.js
@@ -1,23 +1,32 @@
 const express = require('express');
-const path = require('path')
 const app = express();
 
 require('dotenv').config();
-const {sequelize} = require('./models');
 
+const {sequelize} = require('./models');
 async function main(){
 	await sequelize.sync({force : true});
 }
 main();
 
+const path = require('path')
 app.set('view engine' , 'ejs')
 app.set('views' , path.join(__dirname , 'views'));
 
 app.listen(process.env.PORT , () =>{
 	console.log('Listening');
-	console.log(process.env.NODE_ENV);
 })
+const morgan = require('morgan');
+app.use(morgan('tiny'));
 
 app.get('/' , (req , res) =>{
 	res.render('index')
 })
+app.get('/cat' , async (req, res, next) =>{
+	next(new Error("This is a error"));
+})
+
+// app.use((err, req, res , next) =>{
+// 	console.log(err?.message);
+// 	res.status(401).send('<h1>Hello Error</h1>');	
+// })
