e4df362dafd21e66a0205453f5186d98d7880cdc
Ronald Jr Ombao <rombao@my.centennialcollege.ca>
2022-05-14 08:07:43 -0400

    Set up Session Store using sequelize

--- app.js
+++ app.js
@@ -1,6 +1,30 @@
+const { log } = require("console");
 const express = require("express");
+const session = require('express-session');
 const app = express();
 const path = require("path");
+const { sequelize } = require("./models/index");
+const dbstore = require('./models/index');
+const flash = require('connect-flash');
+
+//Session set-up using sequelize store
+var SequelizeStore = require('connect-session-sequelize')(session.Store);
+const store = new SequelizeStore({
+  db: dbstore.sequelize,
+  checkExpirationInterval: 15 * 60 * 1000,
+  expiration: 24 * 60 * 60 * 1000
+});
+app.set('trust proxy', 1);
+app.use(session({
+  secret: 'this is not a secure secret',
+  resave: false,
+  saveUninitialized: true,
+  //Cookie will not save if not https
+  cookie: { secure: false },
+  store: store
+}))
+store.sync();
+app.use(flash());
 
 // Set Up View Engine
 app.set("view engine", "ejs");
@@ -16,16 +40,26 @@ if (process.env.NODE_ENV === "development") {
   app.use(morgan("tiny"));
 }
 
+//flash middleware
+app.use((req , res , next) =>{
+  res.locals.messages = req.flash('success');
+  next();
+})
+
 app.get("/", (req, res) => {
-  res.render("basicUserIndexPage");
+  req.flash('success' , 'Successful trial for flash' );
+  res.redirect('/trial');
 });
 
+app.get('/trial' , (req , res) =>{
+  res.render('trial');
+})
 
 app.use((err, req, res, next) => {
   console.log(err?.message);
 });
 
-let PORT = process.env.PORT || 5000;
+let PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Currently in ${process.env.NODE_ENV} mode`);
   console.log(`Server Active at port ${process.env.PORT}`);
