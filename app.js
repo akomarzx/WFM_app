61da48e2a063f3e055db06a90c925d5b91f9cd75
Ronald Jr Ombao <rombao@my.centennialcollege.ca>
2022-05-15 14:10:57 -0400

    Set-up Partials, flash and added bootstrap and jq.

--- app.js
+++ app.js
@@ -1,59 +1,57 @@
-const { log } = require("console");
-const express = require("express");
+const express = require('express');
 const session = require('express-session');
 const app = express();
-const path = require("path");
-const { sequelize } = require("./models/index");
-const dbstore = require('./models/index');
+const path = require('path');
+const { sequelize } = require('./models/index');
 const flash = require('connect-flash');
+const ejsMate = require('ejs-mate');
 
 //Session set-up using sequelize store
 var SequelizeStore = require('connect-session-sequelize')(session.Store);
 const store = new SequelizeStore({
-  db: dbstore.sequelize,
+  db: sequelize,
   checkExpirationInterval: 15 * 60 * 1000,
-  expiration: 24 * 60 * 60 * 1000
+  expiration: 24 * 60 * 60 * 1000,
 });
 app.set('trust proxy', 1);
-app.use(session({
-  secret: 'this is not a secure secret',
-  resave: false,
-  saveUninitialized: true,
-  //Cookie will not save if not https
-  cookie: { secure: false },
-  store: store
-}))
+app.use(
+  session({
+    secret: 'this is not a secure secret',
+    resave: false,
+    saveUninitialized: true,
+    //Cookie will not save if not https
+    cookie: { secure: false },
+    store: store,
+  })
+);
 store.sync();
 app.use(flash());
 
 // Set Up View Engine
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
+app.engine('ejs', ejsMate);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 //Body Parsers
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 //Morgan Logging
-if (process.env.NODE_ENV === "development") {
-  const morgan = require("morgan");
-  app.use(morgan("tiny"));
+if (process.env.NODE_ENV === 'development') {
+  const morgan = require('morgan');
+  app.use(morgan('tiny'));
 }
 
 //flash middleware
-app.use((req , res , next) =>{
-  res.locals.messages = req.flash('success');
+app.use((req, res, next) => {
+  res.locals.success = req.flash('success');
+  res.locals.error = req.flash('error');
   next();
-})
-
-app.get("/", (req, res) => {
-  req.flash('success' , 'Successful trial for flash' );
-  res.redirect('/trial');
 });
 
-app.get('/trial' , (req , res) =>{
-  res.render('trial');
-})
+app.get('/', (req, res) => {
+  res.render('index');
+});
 
 app.use((err, req, res, next) => {
   console.log(err?.message);
