9a2c512ee7ab2473823179a843558d0a47fb3067
Ronald Jr Ombao <rombao@my.centennialcollege.ca>
2022-06-05 16:22:30 -0400

    Registration is currently working

--- controller/authController.js
+++ controller/authController.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { PunchInfo, User } = require('../models');
 const bcrypt = require('bcrypt');
-
+const { Op } = require('sequelize');
 let get_login_page = (req, res) => {
   res.render('../views/authViews/loginPage');
 };
@@ -18,9 +18,16 @@ let register_user = async (req, res) => {
   // Use the punch info as the registration code and check email in user db to check if email already exists
   // so that only employees can register
   // Add server side validation afterwards
+
   try {
     let punch_info = await PunchInfo.findByPk(regCode);
-    let user = await PunchInfo.findByPk(email);
+    let user = await User.findOne({where : {
+      [Op.or] : [
+        {email : email},
+        {emp_id : punch_info.emp_id}
+      ]
+    }});
+    
     if (punch_info == null || user != null) {
       req.flash(
         'error',
