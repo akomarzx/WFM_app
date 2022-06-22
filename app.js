e9821058491a8b2c7206001fc5fd38b46ef0c03a
Ronald Jr Ombao <rombao@my.centennialcollege.ca>
2022-06-22 18:28:29 -0400

    Try out sequelize transaction

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const passport = require('passport');
 const methodOverride = require('method-override');
 // eslint-disable-next-line no-unused-vars
 const Services = require('./src/services');
+
 // Morgan and live Reloading
 // Disable http caching to live reload css and js
 if (process.env.NODE_ENV === 'development') {
--- src/services/employeeServices.js
+++ src/services/employeeServices.js
@@ -3,12 +3,16 @@ const EventEmitter = require('events');
 const employeeEvents = new EventEmitter();
 const moment = require('moment');
 
-const {Employee, Department, Position} = require('../models');
+const {Employee, Department, Position, sequelize} = require('../models');
 
 const getEmployees = async () => {
   try {
-    const employees = await Employee.findAll({include: [Department, Position]});
-    return employees;
+    const result = sequelize.transaction(async (t) => {
+      const employees = await Employee.findAll(
+          {include: [Department, Position]});
+      return employees;
+    });
+    return result;
   } catch (error) {
     throw new Error(error.message);
   }
@@ -16,17 +20,20 @@ const getEmployees = async () => {
 
 const getEmployee = async (id) => {
   try {
-    const employee = await Employee.findOne({
-      where: {
-        uuid: id,
-      },
+    const result = sequelize.transaction(async (t) => {
+      const employee = await Employee.findOne({
+        where: {
+          uuid: id,
+        },
+      });
+      // TODO throw an exception if nothing is found,
+      // Impelement Error objects. Catch matching error thrown
+      if (employee) {
+        return employee;
+      }
+      throw new Error('Employee Not Found!');
     });
-    // TODO throw an exception if nothing is found,
-    // Impelement Error objects. Catch matching error thrown
-    if (employee) {
-      return employee;
-    }
-    throw new Error('Employee Not Found!');
+    return result;
   } catch (error) {
     throw new Error(error.message);
   }
@@ -34,32 +41,8 @@ const getEmployee = async (id) => {
 
 const createEmployee = async (employeeData) => {
   try {
-    const newEmployee = await Employee.create({
-      dept_id: employeeData.dept_id,
-      super_id: employeeData.super_id,
-      position_id: employeeData.position_id,
-      first_name: employeeData.first_name,
-      last_name: employeeData.last_name,
-      birth_date: moment(employeeData.birth_date, ('YYYY-MM-DD'), true),
-      sex: employeeData.sex,
-      employment_status: employeeData.employment_status,
-    });
-    employeeEvents.emit('employeeCreated', newEmployee);
-    return newEmployee;
-  } catch (error) {
-    throw new Error(error.message);
-  }
-};
-
-const updateEmployee = async (id, employeeData) => {
-  try {
-    const employeeToBeUpdated = await Employee.findOne({
-      where: {
-        uuid: id,
-      },
-    });
-    if (employeeToBeUpdated != null) {
-      await employeeToBeUpdated.set({
+    const result = await sequelize.transaction(async (t) => {
+      const newEmployee = await Employee.create({
         dept_id: employeeData.dept_id,
         super_id: employeeData.super_id,
         position_id: employeeData.position_id,
@@ -68,12 +51,41 @@ const updateEmployee = async (id, employeeData) => {
         birth_date: moment(employeeData.birth_date, ('YYYY-MM-DD'), true),
         sex: employeeData.sex,
         employment_status: employeeData.employment_status,
-      });
-      await employeeToBeUpdated.save();
+      }, {transaction: t});
+      employeeEvents.emit('employeeCreated', newEmployee);
+      return newEmployee;
+    });
+    return result;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
 
-      return employeeToBeUpdated;
-    }
-    throw new Error('Resource not found!');
+const updateEmployee = async (id, employeeData) => {
+  try {
+    const result = await sequelize.transaction(async (t) => {
+      const employeeToBeUpdated = await Employee.findOne({
+        where: {
+          uuid: id,
+        },
+      });
+      if (employeeToBeUpdated != null) {
+        await employeeToBeUpdated.set({
+          dept_id: employeeData.dept_id,
+          super_id: employeeData.super_id,
+          position_id: employeeData.position_id,
+          first_name: employeeData.first_name,
+          last_name: employeeData.last_name,
+          birth_date: moment(employeeData.birth_date, ('YYYY-MM-DD'), true),
+          sex: employeeData.sex,
+          employment_status: employeeData.employment_status,
+        });
+        await employeeToBeUpdated.save();
+        return employeeToBeUpdated;
+      }
+      throw new Error('Resource not found!');
+    });
+    return result;
   } catch (error) {
     throw new Error(error.message);
   }
@@ -81,17 +93,19 @@ const updateEmployee = async (id, employeeData) => {
 
 const deleteEmployee = async (id) => {
   try {
-    const employeeToBeDeleted = await Employee.findOne({
-      where: {
-        uuid: id,
-      },
+    await sequelize.transaction(async (t) => {
+      const employeeToBeDeleted = await Employee.findOne({
+        where: {
+          uuid: id,
+        },
+      });
+      if (employeeToBeDeleted != null) {
+        await employeeToBeDeleted.destroy();
+        employeeEvents.emit('employeeDeleted', employeeToBeDeleted);
+        return;
+      }
+      throw new Error('Resource Not Found!');
     });
-    if (employeeToBeDeleted != null) {
-      employeeEvents.emit('employeeDeleted', employeeToBeDeleted);
-      await employeeToBeDeleted.destroy();
-      return;
-    }
-    throw new Error('Resource Not Found!');
   } catch (error) {
     throw new Error(error.message);
   }
